dad77dfd4f959d9d6801a655bf68739d1bea8f02
Zaman-Pallob <atikurzamanpallob@gmail.com>
2025-05-13 16:37:37 +0600

    assets added

---
+++ lib/core/common_widget/circular_avater.dart
@@ -0,0 +1,44 @@
+import 'package:apptest/core/utils/app_images.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CircularAvater extends StatelessWidget {
+  final String imageUrl;
+  final double width, height;
+  const CircularAvater({
+    super.key,
+    required this.imageUrl,
+    required this.height,
+    required this.width,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          border:
+              Border.all(width: 1.w, color: Colors.teal.withValues(alpha: 0.1)),
+          image: DecorationImage(
+            image: getImage(imageUrl),
+            fit: BoxFit.contain,
+          ),
+          shape: BoxShape.circle),
+    );
+  }
+
+  ImageProvider getImage(String imageUrl) {
+    try {
+      if (imageUrl == "" || imageUrl.contains("null")) {
+        return AssetImage(AppImages.avater);
+      } else {
+        return CachedNetworkImageProvider(imageUrl);
+      }
+    } catch (e) {
+      return AssetImage(AppImages.avater);
+    }
+  }
+}
---
+++ lib/presentation/widgets/user_row_item.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class UserRowItem extends StatelessWidget {
+  final String? name;
+  final String? avaterUrl;
+  const UserRowItem({
+    super.key,
+    this.name,
+    this.avaterUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5.r),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withValues(alpha: 0.4),
+            spreadRadius: 1,
+            blurRadius: 5,
+            offset: const Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundImage: NetworkImage(avaterUrl ?? ''),
+          ),
+          const SizedBox(width: 10),
+          Text(
+            name ?? '',
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+          ),
+        ],
+      ),
+    );
+  }
+}
